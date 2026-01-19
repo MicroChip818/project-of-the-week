e4a2cc2bf48a208b2358d2bc67ce36c5892b261d
MicroChip818 <ekhoock10@gmail.com>
2026-01-19 11:12:10 -0800

    Refactor scramble_cube to support multiple scrambles

--- 2026/01-18-rubiks-cube-scramble-generator.py
+++ 2026/01-18-rubiks-cube-scramble-generator.py
@@ -1,22 +1,28 @@
 import random
-def scramble_cube(moves, moves_per_line):
+import sys
+
+def scramble_cube(moves_per_scramble, moves_per_line, num_scrambles):
     faces = ["F", "B", "U", "D", "L", "R"]
     modifiers = ["", "'", "2"]
-    scramble = ""
-    prev = ""
-    face = ""
-    newline = ""
-    for i in range(moves):
-        if (i + 1) % moves_per_line == 0:
-            newline = "\n"
-        else:
-            newline = ""
-        while face == prev:
-            face = faces[random.randint(0, 5)]
-        mod = modifiers[random.randint(0, 2)]
-        scramble += f"{face}{mod} {newline}"
-        prev = face
-    print(scramble)
+    for i in range(num_scrambles):
+        scramble = ""
+        prev = ""
+        face = ""
+        newline = ""
+        for i in range(moves):
+            if (i + 1) % moves_per_line == 0:
+                newline = "\n"
+            else:
+                newline = ""
+            while face == prev:
+                face = faces[random.randint(0, 5)]
+            mod = modifiers[random.randint(0, 2)]
+            scramble += f"{face}{mod} {newline}"
+            prev = face
+        print(scramble)
+        print()
 
 if __name__ == "__main__":
-    scramble_cube(20, 5)
+    moves_per_scramble = int(sys.argv[1])
+    num_scrambles   = int(sys.argv[2])
+    moves_per_line  = int(sys.argv[3])
