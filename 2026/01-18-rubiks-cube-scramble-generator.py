e4012905e332cf49682669113194e7b0683fb38d
MicroChip818 <ekhoock10@gmail.com>
2026-01-19 12:04:11 -0800

    Add comments for clarity in scramble generator

--- 2026/01-18-rubiks-cube-scramble-generator.py
+++ 2026/01-18-rubiks-cube-scramble-generator.py
@@ -1,33 +1,33 @@
 import random
 import sys
 
-def scramble_cube(moves_per_scramble, num_scrambles, moves_per_line):
-    faces = ["F", "B", "U", "D", "L", "R"]
-    modifiers = ["", "'", "2"]
-    for i in range(num_scrambles):
+def scramble_cube(moves_per_scramble, num_scrambles, moves_per_line): # If you want to copy the code, copy lines 1 and 4-23, then call the function with your own parameters
+    faces = ["F", "B", "U", "D", "L", "R"] # All 6 possible faces
+    modifiers = ["", "'", "2"] # 2 possible modifiers
+    for i in range(num_scrambles): # Repeat this for every scramble
         scramble = ""
         prev = ""
         face = ""
         newline = ""
-        for i in range(moves_per_scramble):
-            if (i + 1) % moves_per_line == 0:
+        for i in range(moves_per_scramble): # Repeat this for every move per scramble
+            if (i + 1) % moves_per_line == 0: # New line after a certain number of movesww
                 newline = "\n"
             else:
                 newline = ""
-            while face == prev:
+            while face == prev: # Ensures the next move uses a different face
                 face = faces[random.randint(0, 5)]
             mod = modifiers[random.randint(0, 2)]
             scramble += f"{face}{mod} {newline}"
             prev = face
         print(scramble)
-        print()
+        print() # New line to separate scrambles
 
-if __name__ == "__main__":
+if __name__ == "__main__": # The following code is only necessary in a GitHub Workflows Project combined with a YAML file
     DEFAULT_MOVES_PER_SCRAMBLE = 20
     DEFAULT_NUM_SCRAMBLES = 3
     DEFAULT_MOVES_PER_LINE = 5
 
-    def get_int(arg_index, default, name):
+    def get_int(arg_index, default, name): # Custom argument inputs
         try:
             return int(sys.argv[arg_index])
         except IndexError:
@@ -40,4 +40,4 @@ if __name__ == "__main__":
     num_scrambles = get_int(2, DEFAULT_NUM_SCRAMBLES, "number of scrambles")
     moves_per_line = get_int(3, DEFAULT_MOVES_PER_LINE, "moves per line")
 
-    scramble_cube(moves_per_scramble, num_scrambles, moves_per_line)
+    scramble_cube(moves_per_scramble, num_scrambles, moves_per_line) # Call the function with required arguments
