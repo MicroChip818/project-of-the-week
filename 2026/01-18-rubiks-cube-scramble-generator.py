fa6221ad81962db2e47c5bb50c1b55c89fb75dff
MicroChip818 <ekhoock10@gmail.com>
2026-01-19 11:49:54 -0800

    Refactor scramble_cube function parameters and input handling

--- 2026/01-18-rubiks-cube-scramble-generator.py
+++ 2026/01-18-rubiks-cube-scramble-generator.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-def scramble_cube(moves_per_scramble, moves_per_line, num_scrambles):
+def scramble_cube(moves_per_scramble, num_scrambles, moves_per_line):
     faces = ["F", "B", "U", "D", "L", "R"]
     modifiers = ["", "'", "2"]
     for i in range(num_scrambles):
@@ -23,6 +23,21 @@ def scramble_cube(moves_per_scramble, moves_per_line, num_scrambles):
         print()
 
 if __name__ == "__main__":
-    moves_per_scramble = int(sys.argv[1])
-    num_scrambles   = int(sys.argv[2])
-    moves_per_line  = int(sys.argv[3])
+    DEFAULT_MOVES_PER_SCRAMBLE = 20
+    DEFAULT_NUM_SCRAMBLES = 3
+    DEFAULT_MOVES_PER_LINE = 5
+
+    def get_int(arg_index, default, name):
+        try:
+            return int(sys.argv[arg_index])
+        except IndexError:
+            return default
+        except ValueError:
+            print(f"Invalid value for {name}. Must be an integer.")
+            sys.exit(1)
+
+    moves_per_scramble = get_int(1, DEFAULT_MOVES_PER_SCRAMBLE, "moves per scramble")
+    num_scrambles = get_int(2, DEFAULT_NUM_SCRAMBLES, "number of scrambles")
+    moves_per_line = get_int(3, DEFAULT_MOVES_PER_LINE, "moves per line")
+
+    scramble_cube(moves_per_scramble, moves_per_line, num_scrambles)
